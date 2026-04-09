965c69f84e8530047044661c45795afa9f5cb157
yadavmk0001-sys <yadavmk0001@gmail.com>
2026-04-09 21:49:01 +0530

    Add files via upload

---
+++ Prog3.py
@@ -0,0 +1,17 @@
+print("Enter marks obtained in 7 subjects out of 80 : ")
+
+maths = 80
+eng = 74
+sst = 79
+sc = 79
+hindi = 78
+comp = 74
+sanskrit = 80
+
+sum = maths + eng + sst + sc + hindi + comp + sanskrit
+print("Sum of all subjects are ",sum)
+
+perc = (sum/400)*100
+
+print(end="Percentage Mark = ")
+print(perc)
